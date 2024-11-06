53378f9ce2024da5a2ab72a0680d566a1845e88c
CALLMESENPAI1 <johncubelo894@gmail.com>
2024-11-06 09:55:22 +0800

    Add files via upload

---
+++ ScrapbookQuistion.java
@@ -0,0 +1,63 @@
+
+package scrapbook.quistion;
+
+import java.util.Scanner;
+
+
+public class ScrapbookQuistion {
+
+    
+    public static void main(String[] args) {
+    //output//
+   System.out.println("Welcome to scrapbook quistions");
+    Scanner sc = new Scanner (System.in); 
+    System.out.print("What is your name? ");
+    String one = sc.nextLine();
+    
+    Scanner ac = new Scanner (System.in);
+     System.out.print("What is your age? ");
+    int two = ac.nextInt();
+     
+     Scanner bc = new Scanner (System.in);
+      System.out.print("what is your favorite food? ");
+       String tree = bc.nextLine();
+       
+    Scanner gc = new Scanner (System.in);
+      System.out.print("What is your favorite person? ");
+       String four = gc.nextLine();
+     
+       Scanner bd = new Scanner (System.in);
+        System.out.print("What is your favorite hobby?  " );
+         String five = bd.nextLine();
+         
+     Scanner gb = new Scanner (System.in);
+       System.out.print("Where do you live? ");
+        String six = gb.nextLine();
+        
+        Scanner hb = new Scanner (System.in);
+        System.out.print("How tall are you? ");
+        String seven = hb.nextLine();
+        
+        Scanner fb = new Scanner (System.in);
+          System.out.print("Are you in a relationship?  ");
+            String eight = fb.nextLine();
+            
+            Scanner jj = new Scanner (System.in);
+            System.out.print("What are you doing today? ");
+           String nine = fb.nextLine();
+           
+            
+          Scanner mg = new Scanner ( System.in);
+          System.out.print("Okay that's all for today thank you mwa     ");
+          
+          
+         
+         
+         
+         
+         
+         
+         
+    }
+    
+}
